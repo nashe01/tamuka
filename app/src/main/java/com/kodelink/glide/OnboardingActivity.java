2f63f456f21537fa975501feffd1dfd9215d066b
nashe01 <mupindu021@gmail.com>
2025-10-12 17:14:37 +0200

    type writter effect on tittle and description

--- app/src/main/java/com/kodelink/glide/OnboardingActivity.java
+++ app/src/main/java/com/kodelink/glide/OnboardingActivity.java
@@ -29,7 +29,7 @@ public class OnboardingActivity extends AppCompatActivity implements View.OnClic
     private int currentStep = 1;
     
     // Typewriter animation speed (milliseconds per character)
-    private static final int TYPING_SPEED = 40;
+    private static final int TYPING_SPEED = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
